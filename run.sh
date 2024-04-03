3f8ccd0cd233d3dba46ffc26239fabc994cfc0ed
heilmao <oheilmann5@gmail.com>
2024-04-03 12:58:25 +0100

    Added new submodule

---
+++ run.sh
@@ -0,0 +1,2 @@
+# Set up your environment by sourcing the following file
+source /opt/ros/humble/setup.bash
---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Assumed that ROS2 Humbe and Gazebo Harmonic are installed already!
+
+# Install here if not:
+#   - https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html
+#   - https://gazebosim.org/docs/harmonic/install_ubuntu
+
+# Setup submodules
+git submodule init
+
+# Replace ".bash" with your shell if you're not using bash
+# Possible values are: setup.bash, setup.sh, setup.zsh
+source /opt/ros/humble/setup.bash
