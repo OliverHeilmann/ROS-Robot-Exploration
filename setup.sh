fa392c16d56d2db77986d6d0320d7e9eef769f1c
heilmao <oheilmann5@gmail.com>
2024-04-03 14:23:41 +0100

    general updates

--- setup.sh
+++ setup.sh
@@ -10,3 +10,13 @@ git submodule init
 # Replace ".bash" with your shell if you're not using bash
 # Possible values are: setup.bash, setup.sh, setup.zsh
 source /opt/ros/humble/setup.bash
+
+# Setup Husarian submodule
+sudo apt-get update
+sudo apt install ros-dev-tools
+vcs import modules < modules/rosbot_xl_ros/rosbot_xl/rosbot_xl_hardware.repos
+vcs import modules < modules/rosbot_xl_ros/rosbot_xl/rosbot_xl_simulation.repos
+sudo rosdep init
+export ROS_DISTRO=humble
+rosdep update --rosdistro $ROS_DISTRO
+rosdep install -i --from-path modules --rosdistro $ROS_DISTRO -y
