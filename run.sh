b77061a091d4f1500490e31e8752aa95ebc13de8
heilmao <oheilmann5@gmail.com>
2024-04-06 14:32:29 +0100

    change package folder name to be more generic

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 # build the workspace package 'brightness' only - exit if build fails
-colcon build --packages-select brightness --symlink-install || exit 1
+colcon build --packages-select rosbot --symlink-install || exit 1
 
 # Run the brightness node using ros2 run
 # ros2 run brightness brightness_node --ros-args -r /image:=/camera/color/image_raw
 
 # Run the brightness node using launch file
-ros2 launch brightness brightness.yaml 
+ros2 launch rosbot brightness.yaml 
 
 # Open the Gazebo Simulator in a new terminal
 ROSBOT_SIM
 
 # Open the Rviz2
-rvis2 -d src/brightness/rviz/rosbot.rviz
+rvis2 -d src/rosbot/rviz/rosbot.rviz
