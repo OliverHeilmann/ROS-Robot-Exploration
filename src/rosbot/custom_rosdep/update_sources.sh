428dad444c3bfe94fbc19c6801be3b6e006f0286
heilmao <oheilmann5@gmail.com>
2024-04-16 14:26:21 +0100

    test pipeline

--- src/rosbot/custom_rosdep/update_sources.sh
+++ src/rosbot/custom_rosdep/update_sources.sh
@@ -18,10 +18,10 @@ DEFAULT_ROSDEP_FILE=$(ls $ROSDEP_SOURCES_DIR | grep -m 1 'default')
 # Append the custom path to the default rosdep file
 if [ ! -z "$DEFAULT_ROSDEP_FILE" ]; then
     echo "$FULL_CUSTOM_YAML_PATH" | sudo tee -a "$ROSDEP_SOURCES_DIR/$DEFAULT_ROSDEP_FILE" > /dev/null
-    echo "Custom rosdep source added to $ROSDEP_SOURCES_DIR/$DEFAULT_ROSDEP_FILE"
+    echo "Custom rosdep source \'$FULL_CUSTOM_YAML_PATH\' added to \'$ROSDEP_SOURCES_DIR/$DEFAULT_ROSDEP_FILE\'"
 else
     echo "No default rosdep source file found."
 fi
 
-# Update rosdep to recognize changes
-sudo rosdep update
+# Update rosdep to recognise changes
+rosdep update
