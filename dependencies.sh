be48ae1006432410dbac0d0326051d120ce03377
heilmao <oheilmann5@gmail.com>
2024-04-19 11:28:45 +0100

    install amment package

--- dependencies.sh
+++ dependencies.sh
@@ -37,6 +37,8 @@ retry 3 sudo apt-get install -y \
     python3-colcon-common-extensions \
     python3-vcstool \
     python3-rosdep \
+    python3-ament-package \
+    python3-ament-cmake \
     libopencv-dev \
     ros-$ROS_DISTRO-ament-cmake \
     ros-$ROS_DISTRO-desktop \
